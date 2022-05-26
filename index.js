4945c4483d8dacf045c1ba115b78badccc060db4
pakjihat <insanul.jihat1984@gmail.com>
2022-05-26 13:01:49 +0800

    feat: Ini commitan ke-2

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const salam="Halo, namaku ";
 const nama="Jihat";
 
 console.log(salam+nama);
+// edit ke-2
+const data={
+    salaman:"Halo lagi, namaku ",
+    saya:"Jihat"
+};
+
+const {salaman,saya}=data;
+
+console.log(salaman+saya);
+
+
